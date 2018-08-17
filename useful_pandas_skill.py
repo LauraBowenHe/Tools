22f225a7f56b864c5f7d73c1025d20407f08586a
Bowen He <bhe@ucdavis.edu>
2018-08-17 14:45:11 +0800

    Update useful_pandas_skill.py

--- useful_pandas_skill.py
+++ useful_pandas_skill.py
@@ -1,7 +1,7 @@
 ### This script records some useful pandas skills I have met or used, record and share :)
 ### This will update constantly
 
-#---------------- handle with nan in column --------------------------------
+#---------------- handle with nan in column ------------------------------
 ### replace outlier value as nan
 df[col].replace(365243, np.nan, inplace= True)
 
@@ -17,7 +17,7 @@ print("fill missing data with mode...")
 for col in df.columns:
     df[col].fillna(df[col].mode()[0], inplace=True)
  
-#---------------- check zero variance variable --------------------------------
+#---------------- check zero variance variable --------------------------
 ### zero variance variable is imprevious to tree models, but not to linear models
 feats_var_zero = []
 df_var = df.var(axis=0)
@@ -26,18 +26,18 @@ for col in df.columns:
         feats_var_zero.append(col)
 df.drop(feats_var_zero, axis=1)
 
-#---------------------- lambda function ---------------------------------------
+#---------------------- lambda function ---------------------------------
 df['z_dist'] = df.apply(lambda x:Levenshtein.ratio(x['question1'], x['question2']), axis=1)
 
-#------------------- get random samples from dataframe ------------------------
+#------------------- get random samples from dataframe ------------------
 ### frac specifys the fraction of rows to return in the random sample
 df.sample(frac=1)
 
-#------------------- parquet for faster save than csv  ------------------------
+#------------------- parquet for faster save than csv  ------------------
 ### (pip install pyarrow)
 df = pd.read_parquet('../data/spn_nlp_feats.parquet.gzip', engine='pyarrow')
 
-#------------------- modify column by certain condition -----------------------
+#------------------- modify column by certain condition -----------------
 df.loc[((df[col]==1) & (df[col1]==1)), 'prob'] = 1
 
 
